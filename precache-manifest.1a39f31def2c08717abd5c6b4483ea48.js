d5947d3330d1a1c4fdc4870cbbab119677d51c68
bennettpe <37184112+bennettpe@users.noreply.github.com>
2019-11-29 10:07:50 +0000

    Updates

---
+++ precache-manifest.1a39f31def2c08717abd5c6b4483ea48.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b56b499abb03000a2172fedd177d7037",
+    "url": "/Monsters-Rolodex/index.html"
+  },
+  {
+    "revision": "c8ad9001ead059d94329",
+    "url": "/Monsters-Rolodex/static/css/main.fa8bfb27.chunk.css"
+  },
+  {
+    "revision": "59c13745251206e2f1b0",
+    "url": "/Monsters-Rolodex/static/js/2.0729de66.chunk.js"
+  },
+  {
+    "revision": "c8ad9001ead059d94329",
+    "url": "/Monsters-Rolodex/static/js/main.90354aef.chunk.js"
+  },
+  {
+    "revision": "11e2dea3cac39d985a42",
+    "url": "/Monsters-Rolodex/static/js/runtime-main.3ef3660b.js"
+  }
+]);
