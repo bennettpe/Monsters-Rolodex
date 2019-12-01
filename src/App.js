4375b58abbe68c4df585f024a61285f781de12b1
bennettpe <37184112+bennettpe@users.noreply.github.com>
2019-12-01 09:53:30 +0000

    46. Revisiting VirtualDOM + Unidirectional Data Flow Changes

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
+
 import { CardList } from './components/card-list/card-list.component';
 import { SearchBox } from './components/search-box/search-box.component';
+
 import './App.css';
 
 // Monsters App
@@ -10,38 +12,41 @@ class App extends Component {
 
     this.state = {
       monsters: [],
-      searchField: ''
+      searchField: '',
+      title: ''
     };
-
     this.handleChange = this.handleChange.bind(this);
   }
 
   componentDidMount() {
-    fetch(' https://jsonplaceholder.typicode.com/users')
-    .then(response => response.json())
-    .then(users => this.setState({ monsters: users }));
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then(response => response.json())
+      .then(users => this.setState({ monsters: users }));
   }
 
   handleChange(e) {
-    this.setState({ searchField: e.target.value });
+    this.setState({ searchField: e.target.value,
+                          title: e.target.value });
   }
-  
+
   render() {
-    const { monsters, searchField } = this.state;
-    const filteredMonsters = monsters.filter(monster =>
-      monster.name.toLowerCase().includes(searchField.toLowerCase())
-      );
+    const { monsters, searchField, title } = this.state;
+    //const filteredMonsters = monsters.filter(monster =>
+    //monster.name.toLowerCase().includes(searchField.toLowerCase())
+    //);
 
     return (
-    <div className='App'>
-      <h1>Monsters Rolodex</h1>
-      <SearchBox
+      <div className='App'>
+        {/*<h1>Monsters Rolodex</h1>*/}
+        <h1>{title}</h1>
+        <SearchBox
         placeholder='search monsters'
         handleChange={this.handleChange}
       />
-      {/*Create Monster Cards*/}
-      <CardList monsters={filteredMonsters} /> 
-    </div>
+        {/*Create Monster Cards*/}
+     {/*<CardList monsters={filteredMonsters} />*/} 
+        <CardList monsters={monsters} /> 
+      </div>
     );
   }
 }
